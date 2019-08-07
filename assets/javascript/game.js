1c7795798873353b0ab77c479e33097320b496d7
Scottie <avscott@pacbell.net>
2019-08-06 22:26:12 -0700

    add pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+
+//establish variables
+//need words array for computer to select from
+//need array to capture guessed letters
+//need variable to capture key press
+
+//game play
+//computer randomly select word
+//display word with an underscore for each letter
+//use onkeyup to get pressed letter from player
+//verify pressed key is a letter and not a number or special character
+//if pressed key is a letter, check to see if letter is in word
+//if letter is in word, change underscore in word for each appearance of the letter to the letter
+//if letter is not in word, lower remainaing guesses count by 1.
+//add guessed letter to guessed letters field
+//once word guessed, up wins count by 1
+//if word not guessed before remaining guesses = 0, notify game over and reveal word.

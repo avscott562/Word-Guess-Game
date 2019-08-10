2d64341da5f5b903538881393972f0056c830632
Scottie <avscott@pacbell.net>
2019-08-10 00:08:57 -0700

    add code to change found letters and reduce guesses remaining for wrong letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,9 @@
 var words = ["zoo", "my", "tester", "my first game", "tree", "why do you ask", "practice", "we", "is", "thinking", "lesson", "random", "xylophone", "dynamic", "wonderful", "enjoy", "amazed", "test this word"];
 
 //current word being solved
-var puzzle;
+var puzzle = "";
 
+//array for puzzle to display as underscore
 var convertedWord = [];
 
 //grab current word id
@@ -32,11 +33,8 @@ var winCount = 0;
 //grab win id
 var wins = document.getElementById("win");
 
-//game play
-
+//valid letters
 var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var start = 0;
 
 // start game
 function newGame() {
@@ -44,42 +42,39 @@ function newGame() {
         //computer randomly selects word
         var computerGuess = words[Math.floor(Math.random() * words.length)];
 
+        //set puzzle to computers chosen word
         puzzle = computerGuess;
     
         //convert word to underscore
         for (i=0; i<computerGuess.length; i++) {
-            puzzle = computerGuess;
+            //check if there is a space in the computer word
             if (computerGuess[i] === " ") {
+                //display space in html
                 convertedWord.push('\xa0');
-                console.log(convertedWord);
             } else {
+                //if not a space, display letter as underscore
                 convertedWord.push("_");
             }
-            console.log(puzzle);
         }
-        currentWord.textContent = convertedWord.join(" ");
-    
-        console.log(computerGuess + " " + computerGuess.length);
 
+        //display convertedWord on html
+        currentWord.textContent = convertedWord.join(" ");
+        console.log("new game " + puzzle);
 }
 
+//initiate game
 newGame();
 
-// user guess
-
-// function guessEval() {
-
-// }
-
+//track players keystrokes
 document.onkeyup = function(event) {
 
     //capture letter pressed by player
     var userGuess = event.key;
 
-    //checking if key pressed is listed in available letters
+    //checking if key pressed is a letter and is available
     var letter = alpha.indexOf(userGuess.toUpperCase());
 
-    //if in available letters array,
+    //if letter available...
     if (letter !== -1) {
 
         //add to guessed letters array
@@ -90,6 +85,26 @@ document.onkeyup = function(event) {
 
         //remove from available letter list
         alpha.splice(letter, 1);
+
+        //check if letter is in puzzle
+        if (puzzle.includes(userGuess.toLowerCase())) {
+            //if in puzzle, loop through puzzle to find where
+            for (p=0; p<puzzle.length; p++) {
+                if (puzzle.charAt(p) == userGuess.toLowerCase()) {
+                    //change convertedWord to display letter in all positions
+                    convertedWord[p] = userGuess.toUpperCase();
+                    //display new convertedWord on screen
+                    currentWord.textContent = convertedWord.join(" ");
+                } 
+            }
+        } else {
+            remaining--
+            guesses.textContent = remaining;
+            if (remaining === 0) {
+                alert("Game Over!  Try Again?")
+                newGame();
+            }
+        }
 
         // puzzle = "delete";
 
